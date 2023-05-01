0e772a1921a6b32690238c950936b18ee2e95462
Nikita Umerenkov <nekit2000basket@mail.ru>
2023-05-01 15:22:40 +0800

    3 семинар

---
+++ sem3/App.java
@@ -0,0 +1,86 @@
+package sem3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import sem3.StudentDomen.Student;
+import sem3.StudentDomen.StudentGroup;
+import sem3.StudentDomen.StudentStream;
+import sem3.StudentDomen.User;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		User u1 = new User("Сергей", "Иванов", 25);
+		Student s1 = new Student("Сергей", "Иванов", 22, (long) 101);
+		Student s2 = new Student("Андрей", "Сидоров", 22, (long) 111);
+		Student s3 = new Student("Иван", "Петров", 22, (long) 121);
+		Student s4 = new Student("Игорь", "Иванов", 23, (long) 301);
+		Student s5 = new Student("Даша", "Цветкова", 23, (long) 171);
+		Student s6 = new Student("Лена", "Незабудкина", 23, (long) 104);
+		Student s7 = new Student("Student07", "StuLastName07", 8, (long) 667);
+		Student s8 = new Student("Student08", "StuLastName08", 8, (long) 787);
+		Student s9 = new Student("Student09", "StuLastName09", 9, (long) 877);
+
+		List<Student> listStud = new ArrayList<Student>();
+
+		listStud.add(s1);
+		listStud.add(s2);
+		listStud.add(s3);
+		listStud.add(s4);
+		listStud.add(s5);
+		listStud.add(s6);
+
+		// Создание группы с именем "Group - group"
+		StudentGroup group = new StudentGroup("Group - group", listStud, 100);
+
+		List<Student> listStudNewGroup = new ArrayList<Student>();
+		listStudNewGroup.add(s7);
+		listStudNewGroup.add(s8);
+		listStudNewGroup.add(s9);
+
+		// Создание группы с именем "Group - groupNewGroup"
+		StudentGroup groupNewGroup = new StudentGroup("Group - groupNewGroup", listStudNewGroup, 10);
+
+		// for (Student stud : group) {
+		// System.out.println(stud);
+		// }
+
+		// System.out.println("============= после сортировки =============");
+		// Collections.sort(group.getStudents());
+
+		// for (Student stud : group) {
+		// System.out.println(stud);
+		// }
+
+		// System.out.println(u1);
+		// System.out.println(s1);
+		// System.out.println(group);
+		// System.out.println(groupNewGroup);
+
+
+		// Создание потока студентов с именем "MSI"
+		StudentStream lomonosov = new StudentStream("MSI", 1);
+		// Наполнение потока
+		lomonosov.addGroupToStream(group);
+		lomonosov.addGroupToStream(groupNewGroup);
+
+		// Вывод потока студентов переопределенным toString
+		System.out.println(lomonosov);
+
+		// Вывод содержимого потока студентов перебором элементов в цикле
+		for (StudentGroup item : lomonosov) {
+			System.out.println(item);
+		}
+
+		// Сортировка элементов потока студентов по размеру групп
+		Collections.sort(lomonosov.getStudentsGroups());
+
+		System.out.println("After sort");
+		// Повторный вывод отсортированных элементов потока студентов
+		for (StudentGroup item : lomonosov) {
+			System.out.println(item);
+		}
+
+	}
+}
